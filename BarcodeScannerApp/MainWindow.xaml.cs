e2b75ddf8a2ddb5e7348c7b4927ad593d7b45dd9
Máté Iszlai <49722141+mateIszlai@users.noreply.github.com>
2020-11-30 16:59:52 +0100

    Add image view feature

--- BarcodeScannerApp/MainWindow.xaml.cs
+++ BarcodeScannerApp/MainWindow.xaml.cs
@@ -1,28 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BarcodeScannerWPF.Model;
+using Microsoft.Win32;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace BarcodeScannerApp
+namespace BarcodeScannerWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BarcodeData _barcodeData = new BarcodeData { SerialNumber = 0000000000 };
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = _barcodeData;
+            image.Source = new BitmapImage(new Uri(@"/input/gazora.jpg", UriKind.Relative));
+        }
+
+        private void addImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var uri = new Uri(openFileDialog.FileName);
+                image.Source = new BitmapImage(uri);
+            }
         }
     }
 }
