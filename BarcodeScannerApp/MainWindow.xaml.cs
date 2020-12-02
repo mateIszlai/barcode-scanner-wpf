0698a39a5629f80b36d35e21edab747b92742e2b
Máté Iszlai <49722141+mateIszlai@users.noreply.github.com>
2020-12-02 16:17:22 +0100

    Add aspose and spire barcode detection feature

--- BarcodeScannerApp/MainWindow.xaml.cs
+++ BarcodeScannerApp/MainWindow.xaml.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.Win32;
+﻿using Aspose.BarCode.BarCodeRecognition;
+using Microsoft.Win32;
+using Spire.Barcode;
 using System;
 using System.Windows;
 using System.Windows.Media.Imaging;
-using Spire.Barcode;
 
 namespace BarcodeScannerWPF
 {
@@ -18,15 +19,42 @@ namespace BarcodeScannerWPF
 
         private void addImageBtn_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*";
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*"
+            };
             if (openFileDialog.ShowDialog() == true)
             {
                 var uri = new Uri(openFileDialog.FileName);
                 var picture = new BitmapImage(uri);
                 image.Source = picture;
-                var datas = BarcodeScanner.Scan(uri.AbsolutePath);
-               serialNumberText.Text = datas[0];
+                
+
+                //Aspose Barcode reader - Fizetős, de jól működött nálam, az evaluation verziót próbáltam
+               
+                using (BarCodeReader reader = new BarCodeReader(openFileDialog.FileName))
+                {
+                    var asposeResult = reader.ReadBarCodes();
+                    if (asposeResult.Length > 0)
+                    {
+                        var number = asposeResult[0].CodeText;
+                        if (number.Length > 10)
+                            asposeNumberText.Text = number.Substring(0, 10);
+                        else
+                            asposeNumberText.Text = number;
+
+                    }
+                        
+                    else
+                        asposeNumberText.Text = "No barcode found";
+                }
+                
+
+                var spireResult = BarcodeScanner.ScanOne(openFileDialog.FileName);
+                if (spireResult.Length > 0)
+                    spireNumberText.Text = spireResult;
+                else
+                    spireNumberText.Text = "No barcode found";
             }
         }
     }
