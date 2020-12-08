6d308b8c5ddc3bfaebc86fc97fc9964946987e0d
Máté Iszlai <49722141+mateIszlai@users.noreply.github.com>
2020-12-08 15:44:15 +0100

    Create ByteScoutReader static class

---
+++ BarcodeScannerApp/Models/Readers/ByteScoutReader.cs
@@ -0,0 +1,21 @@
+﻿using Bytescout.BarCodeReader;
+
+namespace BarcodeScannerApp.Models.Readers
+{
+    public static class ByteScoutReader
+    {
+        public static string DecodeBarcode(string path)
+        {
+            using Reader reader = new Reader
+            {
+                RegistrationName = "demo",
+                RegistrationKey = "demo"
+            };
+            reader.BarcodeTypesToFind.All1D = true;
+            reader.ReadFromFile(path);
+            if (reader.FoundBarcodes.Length > 0)
+                return reader.FoundBarcodes[0].Value;
+            return "No barcode found";
+        }
+    }
+}
