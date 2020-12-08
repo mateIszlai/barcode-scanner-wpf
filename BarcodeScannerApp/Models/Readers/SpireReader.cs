a221ad420beff0977c1443a2b31fdc97f097712b
Máté Iszlai <49722141+mateIszlai@users.noreply.github.com>
2020-12-08 14:03:51 +0100

    Create SpireReader static class

---
+++ BarcodeScannerApp/Models/Readers/SpireReader.cs
@@ -0,0 +1,18 @@
+﻿using Spire.Barcode;
+
+namespace BarcodeScannerApp.Models.Readers
+{
+    public static class SpireReader
+    {
+        public static string DecodeBarcode(string path)
+        {
+            var result = "Spire : ";
+            var spireResult = BarcodeScanner.ScanOne(path);
+
+            if (spireResult.Length > 0)
+                return result + spireResult;
+
+            return result + "No barcode found";
+        }
+    }
+}
