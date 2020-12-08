874d3b05fd0dc215369d55ab51c70231d0cbf1aa
Máté Iszlai <49722141+mateIszlai@users.noreply.github.com>
2020-12-08 14:03:10 +0100

    Create AsposeReader static class

---
+++ BarcodeScannerApp/Models/Readers/AsposeReader.cs
@@ -0,0 +1,27 @@
+﻿using Aspose.BarCode.BarCodeRecognition;
+
+namespace BarcodeScannerApp.Models.Readers
+{
+    public static class AsposeReader
+    {
+        public static string DecodeBarcode(string path)
+        {
+            using (BarCodeReader reader = new BarCodeReader(path))
+            {
+                var asposeResult = reader.ReadBarCodes();
+                var result = "Aspose : ";
+
+                if (asposeResult.Length > 0)
+                {
+                    var number = asposeResult[0].CodeText;
+                    if (number.Length > 10)
+                        return result + number.Substring(0, 10);
+
+                    return number;
+                }
+
+                return result + "No barcode found";
+            }
+        }
+    }
+}
