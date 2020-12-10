e6dceaf4a2f8927264cb71fe130123fe7c55d01d
Máté Iszlai <49722141+mateIszlai@users.noreply.github.com>
2020-12-10 16:13:49 +0100

    Create VintasoftReader static class

---
+++ BarcodeScannerApp/Models/Readers/VintasoftReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using Vintasoft.Barcode;
+
+namespace BarcodeScannerApp.Models.Readers
+{
+    public static class VintasoftReader
+    {
+        public static void SetUp()
+        {
+           BarcodeGlobalSettings.Register("Mate Iszlai", "iszimate@gmail.com", "2021-01-04", "tPs4Q6WfEquFReJy+unImRQ8tl0BeRx1nkqU4QU9Sf3toYTHvEm17rOVOt3xXcYMLTwZmgub0YYjWMQgvSbj9xsgY1lsniW3+MsQX8RdfHdXGtuQYNY/KOH8f+gRc+FSbBiCbcnw/VFPcTaPSabbhGupL9nZtVB3VAhCF+5D/lQ");
+        }
+
+        public static string DecodeBarcode(string path)
+        {
+            var type = "Vintasoft: ";
+            using BarcodeReader reader = new BarcodeReader();
+            reader.Settings.ScanDirection = ScanDirection.LeftToRight;
+            reader.Settings.ExpectedBarcodes = 1;
+            reader.Settings.ScanInterval = 5;
+            reader.Settings.ScanBarcodeTypes = BarcodeType.Code11 | BarcodeType.Code128 | BarcodeType.Codabar | BarcodeType.Code16K | BarcodeType.Code39 | BarcodeType.Code93 | BarcodeType.Standard2of5;
+            reader.Settings.AutomaticRecognition = true;
+            var results = reader.ReadBarcodes(path);
+            if (results.Length > 0)
+                return type + results[0].Value;
+            return type + "No barcode found";
+        }
+
+    }
+}
