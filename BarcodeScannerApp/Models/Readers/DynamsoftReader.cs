ca68958f8bee0945d2003e1028c08ab62c9a617c
Máté Iszlai <49722141+mateIszlai@users.noreply.github.com>
2020-12-10 16:10:19 +0100

    Create DynamsoftReader static class

---
+++ BarcodeScannerApp/Models/Readers/DynamsoftReader.cs
@@ -0,0 +1,32 @@
+﻿using Dynamsoft.Barcode;
+using System;
+
+namespace BarcodeScannerApp.Models.Readers
+{
+    public static class DynamsoftReader
+    {
+        private static BarcodeReader _barcodeReader = new BarcodeReader("t0068NQAAAEZQ+6k6nQlw7e0kS27TpLG9kboWzxAC5NkqExsTUZODN+BD06FtgippZn2Enx6Xtoe+eSMuCvdb9pxUt8zg0Xg=");
+        public static string DecodeBarcode(string path)
+        {
+            var type = "Dynamsoft: ";
+            try
+            {
+                var results = _barcodeReader.DecodeFile(path, "");
+                if (results.Length > 0)
+                {
+                    var number = results[0].BarcodeText;
+                    if(number.Length > 10)
+                        return type + number.Substring(0, 10);
+
+                    return type + number;
+                }
+            }
+            catch (BarcodeReaderException exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+            return type + "No barcode found";
+        }
+
+    }
+}
