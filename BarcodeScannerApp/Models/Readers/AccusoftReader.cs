1e70d16291d916e02ede1596530bc310e7318901
Máté Iszlai <49722141+mateIszlai@users.noreply.github.com>
2020-12-11 13:22:29 +0100

    Create AccusoftReader static class

---
+++ BarcodeScannerApp/Models/Readers/AccusoftReader.cs
@@ -0,0 +1,20 @@
+﻿using Accusoft.BarcodeXpressSdk;
+using System.Drawing;
+
+namespace BarcodeScannerApp.Models.Readers
+{
+    public static class AccusoftReader
+    {
+        public static string DecodeBarcode(string path)
+        {
+            var type = "Accusoft: ";
+            using BarcodeXpress barcodeXpress = new BarcodeXpress();
+            using Bitmap image = new Bitmap(path);
+            var results = barcodeXpress.reader.Analyze(image);
+            if (results.Length > 0)
+                return type + results[0].BarcodeValue;
+            return type + "No barcode found";
+        }
+
+    }
+}
